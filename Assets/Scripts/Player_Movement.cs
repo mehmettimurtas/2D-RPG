1003aef4a47fb44bdfed981b2ab5f6f9ebd34068
Zoretanin <hellbourne35@hotmail.com>
2026-05-01 19:19:08 +0300

    map

---
+++ Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Tilemaps;
+public class Player_movement : MonoBehaviour
+{
+    public float speed = 5f;
+    public Rigidbody2D rb;
+    public Animator animator;
+    public int facingDirection = 1;
+    private void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical).normalized;
+        rb.linearVelocity = movement * speed;
+        animator.SetFloat("horizontal", Mathf.Abs(moveHorizontal));
+        animator.SetFloat("vertical", Mathf.Abs(moveVertical));
+
+        if (moveHorizontal > 0 && transform.localScale.x < 0 || moveHorizontal < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
+
+    }
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+
+}
