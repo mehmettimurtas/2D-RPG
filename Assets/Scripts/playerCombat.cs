bd457a56e9f7320eacb4cf16814ba24467290a82
Zoretanin <hellbourne35@hotmail.com>
2026-05-21 21:43:25 +0300

    playerAttack

--- Assets/Scripts/playerCombat.cs
+++ Assets/Scripts/playerCombat.cs
@@ -3,11 +3,27 @@ using UnityEngine;
 public class playerCombat : MonoBehaviour
 {
     public Animator anim;
+    public float cooldown = 1.5f;
+    private float timer = 0f;
+
+    private void Update()
+    {
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+
+    }
 
 
     public void playerAttack()
     {
-        anim.SetBool("isAttacking", true);
+        if (timer <= 0)
+        {
+            anim.SetBool("isAttacking", true);
+            timer = cooldown;
+        }
+        
     }
 
     public void finishAttack()
